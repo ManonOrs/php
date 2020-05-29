def8eb824cb4ee8ffbeb12d1cd504bf52b6f50b0
33695 <33695@DESKTOP-9T3U4JI>
2020-05-29 17:33:18 +0200

    add connexion

---
+++ includes/connexion.inc.php
@@ -0,0 +1,8 @@
+<div id = "connexion">
+    <span id="btn_connexion"> Connexion </span>
+<form>
+	<input type="text" name="identifiant" id="identifiant" placeholder="Identifiant" required>
+	<input type="password" name="mdp" id="mdp" placeholder="Mot de passe" required>
+	<input type="Submit" name="connexion" id="connexion
+
+</div>
---
+++ includes/footer.inc.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>&copy;2020 Nolark - Créé par Manon Orsini - Page chargée le <?php
+        setlocale(LC_ALL, 'fr-FR.utf8', 'fra');
+        date_default_timezone_set('Europe/Paris');
+        echo strftime("%A %d %B");
+        ?> à <?php echo strftime("%Hh%M"); ?><p>
+</footer>
