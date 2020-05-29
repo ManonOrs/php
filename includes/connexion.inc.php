18c051c1aeac6293e45446a80b0bd208069fac3c
33695 <33695@DESKTOP-9T3U4JI>
2020-05-29 17:59:25 +0200

    add connexion footer pas casser

--- includes/connexion.inc.php
+++ includes/connexion.inc.php
@@ -3,6 +3,6 @@
 <form>
 	<input type="text" name="identifiant" id="identifiant" placeholder="Identifiant" required>
 	<input type="password" name="mdp" id="mdp" placeholder="Mot de passe" required>
-	<input type="Submit" name="connexion" id="connexion
+	<input type="Submit" name="connexion" id="connexion">
 
 </div>
