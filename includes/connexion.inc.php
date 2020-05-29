70c63aa78977187fb8e2ff2af8192d36b4849a41
33695 <33695@DESKTOP-9T3U4JI>
2020-05-29 18:21:44 +0200

    connexion css

--- includes/connexion.inc.php
+++ includes/connexion.inc.php
@@ -1,8 +1,8 @@
 <div id = "connexion">
-    <span id="btn_connexion"> Connexion </span>
 <form>
+    <span id="lbl_connexion"> Connexion </span></br>
 	<input type="text" name="identifiant" id="identifiant" placeholder="Identifiant" required>
 	<input type="password" name="mdp" id="mdp" placeholder="Mot de passe" required>
-	<input type="Submit" name="connexion" id="connexion">
+	<input type="Submit" name="connexion" id="btn_connexion">
 
 </div>
