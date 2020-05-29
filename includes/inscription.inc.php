d65d689b0fd0a0be29b9f9e57b2176f91c8f9bb3
33695 <33695@DESKTOP-9T3U4JI>
2020-05-29 21:14:47 +0200

    page inscription

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -7,7 +7,7 @@
         <input type="password" name="mdpver" id="mdpver" placeholder="Mot de passe" required>
 	<input type="text" name="nom" id="nom" placeholder="Nom" required>
 	<input type="text" name="prenom" id="prenom" placeholder="Prenom" required>
-	<input type="date" name="daten" id="daten" placeholder="Date de naissance" required>
+	<input type="date" name="datver" id="datver" placeholder="Date de naissance" required>
 	<input type="text" name="adresse" id="adresse" placeholder="Adresse" required>
 	<input type="int" name="cp" id="cp" placeholder="Code postal" required>
 	<input type="text" name="ville" id="ville" placeholder="Ville" required>
@@ -25,8 +25,8 @@ if(isset($_POST['btn_inscription'])){
                 $mdpver = $_POST['mdpver'];
 		$nom = $_POST['nom'];
 		$prenom = $_POST['prenom'];
-		$daten = $_POST['daten'];
-		$Adresse = $_POST['adresse'];
+		$datver = $_POST['datver'];
+		$adresse = $_POST['adresse'];
 		$cp = $_POST['cp'];
 		$ville = $_POST['ville'];
 		$email = $_POST['email'];
@@ -36,5 +36,11 @@ if(isset($_POST['btn_inscription'])){
                 else{
                 echo '<script type="text/javascript">window.alert("Le mot de passe doit etre identique");</script>';
                 }
+                
+                $cnx1 = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd');
+                $req1 = "INSERT INTO `utilisateur` (`identifiant`,`mdp`, `nom`, `prenom`, `email`, `date_naissance`, `adresse`, `ville`, `cp`)
+                        VALUES ('".$identifiant."','".$mdpdef."','".$nom."','".$prenom."','".$email."','".$datver."','".$adresse."','".$ville."','".$cp."')";
+                $res1 = $cnx1->prepare($req1);
+                $res1->execute();
 	}
 ?>
